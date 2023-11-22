28d7a4113401ee049c87a4ee2e41179804877ce2
Andrew <defould@ya.ru>
2023-11-22 19:53:10 +0300

    rewrite app.js by hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,55 +1,55 @@
-import { Component } from 'react';
-
+import { useState } from 'react';
 import './App.css';
 
+import Footer from '../footer/footer';
 import NewTaskForm from '../newTaskForm/newTaskForm';
 import TaskList from '../taskList/taskList';
-import Footer from '../footer/footer';
 
-class App extends Component {
-  minId = 0;
+const App = () => {
+  // state = {
+  //   todoData: [],
+  //   filter: 'all',
+  // };
 
-  state = {
-    todoData: [],
-    filter: 'all',
-  };
+  const [todoData, setTodoData] = useState([]);
+  const [filter, setFilter] = useState('all');
 
-  timers = {};
+  let timers = {};
 
-  addItem = (text, time) => {
+  const addItem = (text, time) => {
     if (!text.trim()) return;
-    const newItem = this.createItem(text, time);
+    const newItem = createItem(text, time);
 
-    this.setState(({ todoData }) => ({
+    setTodoData((todoData) => ({
       todoData: [...todoData, newItem],
     }));
   };
 
-  deleteItem = (id) => {
-    clearInterval(this.timers[id]);
-    delete this.timers[id];
+  const deleteItem = (id) => {
+    clearInterval(timers[id]);
+    delete timers[id];
 
-    this.setState(
+    setTodoData(
       ({ todoData }) => ({
         todoData: todoData.filter((task) => task.id !== id),
       }),
-      this.updateTimers
+      updateTimers
     );
   };
 
-  deleteCompleted = () => {
-    const complitedTasks = this.state.todoData.filter((task) => task.done);
-    complitedTasks.forEach((task) => this.deleteItem(task.id));
+  const deleteCompleted = () => {
+    const complitedTasks = todoData.filter((task) => task.done);
+    complitedTasks.forEach((task) => deleteItem(task.id));
   };
 
-  onToggleDone = (id) => {
-    this.pauseTimer(id);
-    this.setState(({ todoData }) => ({
-      todoData: todoData.map((item) => (item.id === id ? { ...item, done: !item.done, checked: !item.checked } : item)),
-    }));
+  const onToggleDone = (id) => {
+    pauseTimer(id);
+    setTodoData((todoData) =>
+      todoData.map((item) => (item.id === id ? { ...item, done: !item.done, checked: !item.checked } : item))
+    );
   };
 
-  onFilter = (items, filter) => {
+  const onFilter = (items, filter) => {
     switch (filter) {
       case 'active':
         return items.filter((item) => !item.done);
@@ -60,21 +60,21 @@ class App extends Component {
     }
   };
 
-  onFilterSelect = (filter) => {
-    this.setState({ filter });
+  const onFilterSelect = (filter) => {
+    setFilter(filter);
   };
 
-  startTimer = (id) => {
-    if (Object.keys(this.timers).includes(id.toString())) return;
+  const startTimer = (id) => {
+    if (Object.keys(timers).includes(id.toString())) return;
 
-    this.timers[id] = setInterval(() => {
-      this.setState(({ todoData }) => {
+    timers[id] = setInterval(() => {
+      setTodoData(({ todoData }) => {
         const task = todoData.find((item) => item.id === id);
         task.time--;
 
         if (!task.time) {
-          clearInterval(this.timers[id]);
-          delete this.timers[id];
+          clearInterval(timers[id]);
+          delete timers[id];
           task.isTimerOn = false;
         } else {
           task.isTimerOn = true;
@@ -87,65 +87,57 @@ class App extends Component {
     }, 1000);
   };
 
-  pauseTimer = (id) => {
-    clearInterval(this.timers[id]);
-    delete this.timers[id];
+  const pauseTimer = (id) => {
+    clearInterval(timers[id]);
+    delete timers[id];
 
-    this.setState(({ todoData }) => ({
+    setTodoData(({ todoData }) => ({
       todoData: todoData.map((item) => (item.id === id ? { ...item, isTimerOn: false } : item)),
     }));
   };
 
-  updateTimers() {
-    Object.values(this.timers).forEach((timer) => clearInterval(timer));
-    this.timers = {};
+  const updateTimers = () => {
+    Object.values(timers).forEach((timer) => clearInterval(timer));
+    timers = {};
 
-    this.state.todoData.forEach((task) => {
+    todoData.forEach((task) => {
       if (task.isTimerOn) {
-        this.startTimer(task.id);
+        startTimer(task.id);
       }
     });
-  }
+  };
 
-  createItem = (text, time = null) => {
+  const createItem = (text, time = null) => {
     return {
       label: text,
       done: false,
       checked: false,
       created: new Date(),
-      id: this.minId++,
+      id: Math.random().toString(36).slice(2),
       time: time,
       isTimerOn: false,
     };
   };
 
-  render() {
-    const { todoData, filter } = this.state;
-    const visibleData = this.onFilter(todoData, filter);
-    const doneCount = todoData.length - todoData.filter((el) => el.done === true).length;
+  const visibleData = onFilter(todoData, filter);
+  const doneCount = todoData.length - todoData.filter((el) => el.done === true).length;
 
-    return (
-      <div className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <NewTaskForm addItem={this.addItem} />
-        </header>
-        <TaskList
-          todoData={visibleData}
-          onDelete={this.deleteItem}
-          onToggleDone={this.onToggleDone}
-          startTimer={this.startTimer}
-          pauseTimer={this.pauseTimer}
-        />
-        <Footer
-          doneItems={doneCount}
-          deleteCompleted={this.deleteCompleted}
-          filter={filter}
-          onFilterSelect={this.onFilterSelect}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <NewTaskForm addItem={addItem} />
+      </header>
+      <TaskList
+        todoData={visibleData}
+        onDelete={deleteItem}
+        onToggleDone={onToggleDone}
+        startTimer={startTimer}
+        pauseTimer={pauseTimer}
+      />
+      <Footer doneItems={doneCount} deleteCompleted={deleteCompleted} filter={filter} onFilterSelect={onFilterSelect} />
+    </div>
+  );
+};
 
 export default App;
